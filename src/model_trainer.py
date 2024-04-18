0ceb9f40cfb0c3ebf8b341f02f57f41b0338cb4e
Sergey Bendak <sleepoke@gmail.com>
2024-04-18 12:14:08 +0300

    update model trainer

--- src/model_trainer.py
+++ src/model_trainer.py
@@ -1,11 +1,15 @@
 import joblib
+import numpy as np
 import pandas as pd
 
 from sklearn.model_selection import GridSearchCV
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline
-from scipy.stats import cramers_v
+from scipy.stats import chi2_contingency
+
+
+DATASET_PATH = '../data/diabetes_data.csv'
 
 
 def save_model(model, path):
@@ -16,17 +20,42 @@ def load_model(path):
     return joblib.load(path)
 
 
+def create_model(path_to_save):
+    X, y = _load_data(DATASET_PATH)
+    model = _train_model(X, y)
+    save_model(model, path_to_save)
+    return model
+
+
+def predict(model, data):
+    df = pd.DataFrame([data])
+    df['gender'] = df['gender'].map({'Male': 0, 'Female': 1})
+    return bool(model.predict(df)[0])
+
+
+def _cramers_v(x, y):
+    confusion_matrix = pd.crosstab(x, y)
+    chi2 = chi2_contingency(confusion_matrix)[0]
+    n = confusion_matrix.sum().sum()
+    phi2 = chi2 / n
+    r, k = confusion_matrix.shape
+    phi2corr = max(0, phi2 - ((k-1)*(r-1))/(n-1))    
+    rcorr = r - ((r-1)**2)/(n-1)
+    kcorr = k - ((k-1)**2)/(n-1)
+    return np.sqrt(phi2corr / min((kcorr-1), (rcorr-1)))
+
+
 def _calculate_correlation(df, cat_variables, threshold):
     corr_w_target = {}
 
     for variable in cat_variables:
-        corr_w_target[variable] = cramers_v(df[variable], df['class'])
+        corr_w_target[variable] = _cramers_v(df[variable], df['class'])
 
     return [v for v, corr in corr_w_target.items() if corr > threshold]
 
 
 def _load_data(path):
-    df = pd.read_csv('../data/diabetes_data.csv', sep=';')
+    df = pd.read_csv(path, sep=';')
     df['gender'] = df['gender'].map({'Male': 0, 'Female': 1})
 
     cat_var = ['gender', 'polyuria', 'polydipsia', 'sudden_weight_loss',
@@ -35,7 +64,7 @@ def _load_data(path):
                'muscle_stiffness', 'alopecia', 'obesity']
 
     significant_variables = _calculate_correlation(df, cat_var, 0.1)
-    X = df[significant_variables]
+    X = df[['age'] + significant_variables]
     y = df['class']
 
     return X, y
